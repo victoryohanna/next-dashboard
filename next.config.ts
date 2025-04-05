2cedcc6f8252dbdae3fa13e03d786952f2bb7c46
Victor Yohanna <you@example.com>
2025-04-05 14:09:42 +0100

    fix dependency error

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+   webpack: (config) => {
+    config.externals = [...config.externals, 'bcrypt'];
+    return config;
+  }
 };
 
 export default nextConfig;
