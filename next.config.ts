9cf804393b0f50e5d9b94a949f43b7afd40344bf
Victor Yohanna <you@example.com>
2025-04-05 14:20:28 +0100

    replaced bcrypt with bcryptjs

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-   webpack: (config) => {
-    config.externals = [...config.externals, 'bcrypt'];
-    return config;
-  }
 };
 
 export default nextConfig;
